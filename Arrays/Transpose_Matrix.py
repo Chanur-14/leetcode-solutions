4bedd926c952b5de590f0c88857019e4969b75f3
Chanur <chanurpatan@gmail.com>
2026-08-10 21:42:42 +0530

    867 Transpose Matrix

---
+++ Arrays/Transpose_Matrix.py
@@ -0,0 +1,15 @@
+#Leetcode 867 - Transpose Matrix
+#Difficulty: Easy
+#Time Complexity: Time: O(n²)
+#space Complexity: Space: O(n²) 
+from typing import List
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        r = len(matrix)
+        c = len(matrix[0])
+        result = [[0]*r for _ in range(c)]
+        for i in range(r):
+            for j in range(c):
+                result[j][i] = matrix[i][j]
+        return result
+        
