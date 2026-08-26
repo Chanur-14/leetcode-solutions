3342dd048beb3f71aa01bd34c737827f44231c08
Chanur <chanurpatan@gmail.com>
2026-08-26 20:36:18 +0530

    Next Greater Letter 744

---
+++ Arrays/744lp.py
@@ -0,0 +1,19 @@
+#744-Next Greatest Letter
+#Difficulty: Easy
+#Time Complexity: O(log n)  
+#Space Complexity: O(1)
+from typing import List
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        low = 0
+        high = len(letters)-1
+        while low <= high:
+            mid = (low+high)//2
+            if letters[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        if low  == len(letters):
+            return letters[0]
+        return letters[low]
