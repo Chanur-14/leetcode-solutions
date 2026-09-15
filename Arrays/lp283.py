16d44f921b681abd0069fd866fe2062385e5c135
Chanur <chanurpatan@gmail.com>
2026-09-15 18:43:16 +0530

    Merge Sorted Array

---
+++ Arrays/lp283.py
@@ -0,0 +1,11 @@
+#Leetcode no: 283 Move Zeros
+
+from typing import List
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i],nums[j] = nums[j],nums[i]
+                j += 1
+        return nums
